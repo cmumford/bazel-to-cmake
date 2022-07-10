bc9c8b828861cc77d9b689bb3bc09a948b96ba95
Chris Mumford <cmumford@google.com>
2022-07-10 08:04:51 -0700

    Moved hard-coded CMake template into external file.
    Also using Jinja2 to render the template. This is to:
    
    1. Simplify template editing.
    2. Simplify the code if/when this program begins to support
       other export formats (other than CMake).

--- bazel_to_cmake_lib/converter.py
+++ bazel_to_cmake_lib/converter.py
@@ -13,10 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import textwrap
+import jinja2
+import os
 
 from .build_file_functions import BuildFileFunctions
 from .workspace_file_functions import WorkspaceFileFunctions
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 def GetDict(obj):
@@ -40,76 +43,14 @@ class Converter(object):
         self.toplevel = ""
         self.if_lua = ""
 
+    def LoadCMakeTemplate(self):
+        templateLoader = jinja2.FileSystemLoader(searchpath=this_dir)
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        return templateEnv.get_template('cmake_template.txt')
+
     def convert(self):
-        return self.template % {
-            "prelude": self.prelude,
-            "toplevel": self.toplevel,
-        }
-
-    template = textwrap.dedent("""\
-    # This file was generated from BUILD using tools/make_cmakelists.py.
-
-    cmake_minimum_required(VERSION 3.1)
-
-    if(${CMAKE_VERSION} VERSION_LESS 3.12)
-        cmake_policy(VERSION ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION})
-    else()
-        cmake_policy(VERSION 3.12)
-    endif()
-
-    cmake_minimum_required (VERSION 3.0)
-    cmake_policy(SET CMP0048 NEW)
-
-    %(prelude)s
-
-    # Prevent CMake from setting -rdynamic on Linux (!!).
-    SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
-    SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
-
-    # Set default build type.
-    if(NOT CMAKE_BUILD_TYPE)
-      message(STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
-      set(CMAKE_BUILD_TYPE "RelWithDebInfo" CACHE STRING
-          "Choose the type of build, options are: Debug Release RelWithDebInfo MinSizeRel."
-          FORCE)
-    endif()
-
-    # When using Ninja, compiler output won't be colorized without this.
-    include(CheckCXXCompilerFlag)
-    CHECK_CXX_COMPILER_FLAG(-fdiagnostics-color=always SUPPORTS_COLOR_ALWAYS)
-    if(SUPPORTS_COLOR_ALWAYS)
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=always")
-    endif()
-
-    # Implement ASAN/UBSAN options
-    if(UPB_ENABLE_ASAN)
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address")
-      set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address")
-      set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address")
-      set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fsanitize=address")
-    endif()
-
-    if(UPB_ENABLE_UBSAN)
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
-      set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address")
-      set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address")
-      set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fsanitize=address")
-    endif()
-
-    include_directories(.)
-    include_directories(${CMAKE_CURRENT_BINARY_DIR})
-
-    if(APPLE)
-      set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -undefined dynamic_lookup -flat_namespace")
-    elseif(UNIX)
-      set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--build-id")
-    endif()
-
-    enable_testing()
-
-    %(toplevel)s
-
-  """)
+        t = self.LoadCMakeTemplate()
+        return t.render(prelude=self.prelude, toplevel=self.toplevel)
 
     def ConvertDir(self, cmake_out_file):
         execfile("WORKSPACE", GetDict(WorkspaceFileFunctions(self)))
